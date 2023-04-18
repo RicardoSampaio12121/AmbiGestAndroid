d9d96ac1077bf0e42c3c463861b7add12ea0f523
Ricardo José da Costa Sampaio <rsampaio@primeit.pt>
2023-04-18 18:26:42 +0100

    Initial commit

---
+++ app/src/main/java/com/padm/ambigest/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.padm.ambigest.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.padm.ambigest.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
